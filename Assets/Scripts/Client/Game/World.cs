3db22b2d2676e9f9412155480da6bd4af49c7086
Dér Zsombor Zsolt <y07jyd@inf.elte.hu>
2024-04-27 13:31:38 +0200

    Fixed bugs and added some tests for road generation

--- Assets/Scripts/Client/Game/World.cs
+++ Assets/Scripts/Client/Game/World.cs
@@ -3,51 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class World : MonoBehaviour
+namespace Game
 {
-    [SerializeField]
-    private GameObject ChunkPrefab;
-
-    private Chunk[,] Chunks;
-
-    // Start is called before the first frame update
-    private void Start()
+    namespace World
     {
-        Chunks = new Chunk[GameConfig.CHUNK_COUNT, GameConfig.CHUNK_COUNT];
-
-        for (int i = 0; i < GameConfig.CHUNK_COUNT; i++)
+        public class World : MonoBehaviour
         {
-            for (int j = 0; j < GameConfig.CHUNK_COUNT; j++)
-            {
-                LoadChunk(j, i);
-            }
-        }
-    }
+            [SerializeField]
+            private GameObject ChunkPrefab;
 
-    public void LoadChunk(int x, int z)
-    {
-        if (Chunks[z, x] == null)
-        {
-            Chunks[z, x] = Instantiate(ChunkPrefab, this.transform, false).GetComponent<Chunk>();
-            List<EdgeRoadContainer> edges = new List<EdgeRoadContainer>();
-            if (z - 1 >= 0 && Chunks[z - 1, x] != null)
+            private Chunk[,] Chunks;
+
+            // Start is called before the first frame update
+            private void Start()
             {
-                edges.AddRange(Chunks[z - 1, x].EdgeRoads.Where(x => x.EdgeRoad.y == GameConfig.CHUNK_SIZE - 1));
-            }
-            if (z + 1 < GameConfig.CHUNK_COUNT && Chunks[z + 1, x] != null)
-            {
-                edges.AddRange(Chunks[z + 1, x].EdgeRoads.Where(x => x.EdgeRoad.y == 0));
-            }
-            if (x - 1 >= 0 && Chunks[z, x - 1] != null)
-            {
-                edges.AddRange(Chunks[z, x - 1].EdgeRoads.Where(x => x.EdgeRoad.x == GameConfig.CHUNK_SIZE - 1));
-            }
-            if (x + 1 < GameConfig.CHUNK_COUNT && Chunks[z, x + 1] != null)
-            {
-                edges.AddRange(Chunks[z, x + 1].EdgeRoads.Where(x => x.EdgeRoad.x == 0));
+                Chunks = new Chunk[GameConfig.CHUNK_COUNT, GameConfig.CHUNK_COUNT];
+
+                for (int i = 0; i < GameConfig.CHUNK_COUNT; i++)
+                {
+                    for (int j = 0; j < GameConfig.CHUNK_COUNT; j++)
+                    {
+                        LoadChunk(j, i);
+                    }
+                }
             }
 
-            Chunks[z, x].InitChunk(x, z, edges);
+            public void LoadChunk(int x, int z)
+            {
+                if (Chunks[z, x] == null)
+                {
+                    Chunks[z, x] = Instantiate(ChunkPrefab, this.transform, false).GetComponent<Chunk>();
+                    List<EdgeRoadContainer> edges = new List<EdgeRoadContainer>();
+                    if (z - 1 >= 0 && Chunks[z - 1, x] != null)
+                    {
+                        edges.AddRange(Chunks[z - 1, x].EdgeRoads.Where(x => x.EdgeRoad.y == GameConfig.CHUNK_SIZE - 1));
+                    }
+                    if (z + 1 < GameConfig.CHUNK_COUNT && Chunks[z + 1, x] != null)
+                    {
+                        edges.AddRange(Chunks[z + 1, x].EdgeRoads.Where(x => x.EdgeRoad.y == 0));
+                    }
+                    if (x - 1 >= 0 && Chunks[z, x - 1] != null)
+                    {
+                        edges.AddRange(Chunks[z, x - 1].EdgeRoads.Where(x => x.EdgeRoad.x == GameConfig.CHUNK_SIZE - 1));
+                    }
+                    if (x + 1 < GameConfig.CHUNK_COUNT && Chunks[z, x + 1] != null)
+                    {
+                        edges.AddRange(Chunks[z, x + 1].EdgeRoads.Where(x => x.EdgeRoad.x == 0));
+                    }
+
+                    Chunks[z, x].InitChunk(x, z, edges);
+                }
+            }
         }
     }
 }
---
+++ Assets/Tests/RoadGeneratorTests.cs
@@ -0,0 +1,55 @@
+using Game.World;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tests
+{
+    namespace GameTests
+    {
+        public class RoadGeneratorTests
+        {
+            [Test]
+            public void TestGenerationWhenNoStartGiven()
+            {
+                RoadGenerator roadGenerator = new RoadGenerator(GameConfig.CHUNK_SIZE, new List<EdgeRoadContainer>());
+
+                Assert.IsTrue(roadGenerator.EdgeRoads.Count > 0);
+
+                Assert.AreEqual(GameConfig.CHUNK_SIZE / 2, roadGenerator.EdgeRoads[0].EdgeRoad.x);
+                Assert.AreEqual(0, roadGenerator.EdgeRoads[0].EdgeRoad.y);
+            }
+
+            [Test]
+            public void TestGenerationWhenStartWasGiven()
+            {
+                List<EdgeRoadContainer> edgeRoadContainers = new List<EdgeRoadContainer>();
+                edgeRoadContainers.Add(new EdgeRoadContainer(new Vector2Int(10, 0), 1, new Vector2Int(0, -1)));
+                RoadGenerator roadGenerator = new RoadGenerator(GameConfig.CHUNK_SIZE, edgeRoadContainers);
+
+                Assert.IsTrue(roadGenerator.EdgeRoads.Count > 0);
+
+                Assert.AreEqual(10, roadGenerator.EdgeRoads[0].EdgeRoad.x);
+                Assert.AreEqual(GameConfig.CHUNK_SIZE - 1, roadGenerator.EdgeRoads[0].EdgeRoad.y);
+            }
+
+            [Test]
+            public void TestGenerationWhenMultipleStartWasGiven()
+            {
+                List<EdgeRoadContainer> edgeRoadContainers = new List<EdgeRoadContainer>();
+                edgeRoadContainers.Add(new EdgeRoadContainer(new Vector2Int(10, 0), 1, new Vector2Int(0, -1)));
+                edgeRoadContainers.Add(new EdgeRoadContainer(new Vector2Int(0, 5), 1, new Vector2Int(-1, 0)));
+                edgeRoadContainers.Add(new EdgeRoadContainer(new Vector2Int(GameConfig.CHUNK_SIZE - 1, 5), 1, new Vector2Int(1, 0)));
+                RoadGenerator roadGenerator = new RoadGenerator(GameConfig.CHUNK_SIZE, edgeRoadContainers);
+
+                Assert.IsTrue(roadGenerator.EdgeRoads.Count > 2);
+
+                Assert.AreEqual(10, roadGenerator.EdgeRoads[0].EdgeRoad.x);
+                Assert.AreEqual(GameConfig.CHUNK_SIZE - 1, roadGenerator.EdgeRoads[0].EdgeRoad.y);
+                Assert.IsTrue(roadGenerator.EdgeRoads.Exists(x => x.EdgeRoad.x == 0 && x.EdgeRoad.y == 5));
+                Assert.IsTrue(roadGenerator.EdgeRoads.Exists(x => x.EdgeRoad.x == GameConfig.CHUNK_SIZE - 1 && x.EdgeRoad.y == 5));
+            }
+        }
+    }
+}
