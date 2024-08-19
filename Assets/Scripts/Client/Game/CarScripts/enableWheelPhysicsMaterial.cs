5483d1c0f4626df104577ea9582d6020a649c4c3
zsonbi <zsomborder@gmail.com>
2024-08-19 23:22:40 +0200

    Add road friction (#18)
    * Reorganization and friction with ground
    
    * Removed some unused stuff
    
    ---------
    
    Co-authored-by: Dér Zsombor Zsolt <zsombor.der@mptrdev.com>

---
+++ Assets/Scripts/Client/Game/CarScripts/enableWheelPhysicsMaterial.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnableWheelPhysicMaterial : MonoBehaviour
+{
+    private WheelCollider wheel;
+
+    private float originalSidewaysStiffness;
+    private float originalForwardStiffness;
+
+    private void Start()
+    {
+        wheel = GetComponent<WheelCollider>();
+
+        originalSidewaysStiffness = wheel.sidewaysFriction.stiffness;
+        originalForwardStiffness = wheel.forwardFriction.stiffness;
+    }
+
+    // static friction of the ground material.
+    private void FixedUpdate()
+    {
+        WheelHit hit;
+        if (wheel.GetGroundHit(out hit))
+        {
+            WheelFrictionCurve fFriction = wheel.forwardFriction;
+            fFriction.stiffness = hit.collider.material.staticFriction * originalForwardStiffness;
+            wheel.forwardFriction = fFriction;
+
+            WheelFrictionCurve sFriction = wheel.sidewaysFriction;
+            sFriction.stiffness = hit.collider.material.staticFriction * originalSidewaysStiffness;
+            wheel.sidewaysFriction = sFriction;
+        }
+    }
+}
