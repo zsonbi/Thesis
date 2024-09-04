f318bfc280677819513f322624959db6f47c7273
zsonbi <zsomborder@gmail.com>
2024-09-04 10:35:16 +0200

    fixed a possible build issue

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -4,7 +4,7 @@ public static class BuildScript {
     public static void BuildWebGL() {
         // Disable compression
         PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
-
+        
         // Build WebGL
         BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "BuildOutputPath", BuildTarget.WebGL, BuildOptions.None);
     }
