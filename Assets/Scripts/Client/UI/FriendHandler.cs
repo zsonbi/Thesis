33c501adc6d9983cde9c18060fa870fb63b0ed8b
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-09-15 16:21:15 +0200

    Added basic friend panel

---
+++ Assets/Scripts/Client/UI/FriendHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FriendHandler : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private GameObject FriendsContainer;
+
+    [SerializeField]
+    private TMP_InputField taskNameInput;
+
+
+    private bool showPending=true;
+
+    public void ShowPendingChanged(Boolean newValue)
+    {
+        this.showPending = newValue;
+    }
+
+    public void Show()
+    {
+
+        this.gameObject.SetActive(true);
+        LoadFromApi();
+    }
+
+
+    public void SendFriendRequest()
+    {
+
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void LoadFromApi()
+    {
+
+    }
+
+}
