3e2b574f514e0d3ce3de63bf72df2423f5f3f8c7
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-10-06 00:33:42 +0200

    Coloring for the task type in the task history

--- Assets/Scripts/Client/MainPage/DisplayElements/TaskHistoryHandler.cs
+++ Assets/Scripts/Client/MainPage/DisplayElements/TaskHistoryHandler.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using Thesis_backend.Data_Structures;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class TaskHistoryHandler : MonoBehaviour
 {
     [SerializeField]
@@ -23,5 +25,14 @@ public class TaskHistoryHandler : MonoBehaviour
         completeDateText.text = string.Format("{0}. {1:D2}. {2:D2}.", (int)taskHistory.Completed.Year, taskHistory.Completed.Month, taskHistory.Completed.Day);
 
         taskIntervalText.text = ((TaskIntervals)(taskHistory.CompletedTask.PeriodRate)).ToString();
+
+        if (taskHistory.CompletedTask.TaskType)
+        {
+            this.GetComponent<Image>().color = new Color(83f / 255f, 19f / 255f, 0);
+        }
+        else
+        {
+            this.GetComponent<Image>().color = new Color(0f / 255f, 83f / 255f, 0);
+        }
     }
 }
