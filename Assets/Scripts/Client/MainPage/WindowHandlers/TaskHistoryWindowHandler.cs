109c30de0c589931d4e60f67599d3f64d60d29ec
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-10-06 23:47:03 +0200

    Task documentations

--- Assets/Scripts/Client/MainPage/WindowHandlers/TaskHistoryWindowHandler.cs
+++ Assets/Scripts/Client/MainPage/WindowHandlers/TaskHistoryWindowHandler.cs
@@ -1,65 +1,92 @@
 using Config;
-using System;
 using System.Collections.Generic;
 using Thesis_backend.Data_Structures;
-using TMPro;
 using UnityEngine;
-using User;
 
 namespace MainPage
 {
+    /// <summary>
+    /// Task histories window controller
+    /// </summary>
     public class TaskHistoryWindowHandler : ThreadSafeMonoBehaviour
     {
+        /// <summary>
+        /// The container where to put the task histories
+        /// </summary>
         [SerializeField]
         private GameObject taskHistoriesParent;
 
+        /// <summary>
+        /// Reference to the modal window to alert the user for api fails
+        /// </summary>
         [SerializeField]
         private ModalWindow modalWindow;
 
+        /// <summary>
+        /// Task history prefab
+        /// </summary>
         [SerializeField]
         private GameObject taskHistoryItemPrefab;
 
+        /// <summary>
+        /// Shows the task history window
+        /// </summary>
         public void Show()
         {
             this.gameObject.SetActive(true);
             LoadTaskHistories();
         }
 
+        /// <summary>
+        /// Hides the task history window
+        /// </summary>
         public void Hide()
         {
             this.gameObject.SetActive(false);
         }
 
+        /// <summary>
+        /// Loads the task histories from the server
+        /// </summary>
         public void LoadTaskHistories()
         {
             CoroutineRunner.RunCoroutine(Server.SendGetRequest<List<TaskHistory>>(ServerConfig.PATH_FOR_GET_TASK_HISTORIES, LoadedHistories, onFailedAction: ShowRequestFail));
         }
 
+        /// <summary>
+        /// Show the server response errors
+        /// </summary>
+        /// <param name="content">The server response</param>
         private void ShowRequestFail(string content)
         {
             modalWindow.Show("Task history error", content);
         }
 
+        /// <summary>
+        /// When the server response is recieved load the histories
+        /// </summary>
+        /// <param name="taskHistories"></param>
         private void LoadedHistories(List<TaskHistory> taskHistories)
         {
-            if (taskHistoriesParent == null || this.taskHistoriesParent.transform == null)
+            //Protect from errors during testing
+            try
             {
-                // Exit or handle the case when the Score parent is destroyed
-                Debug.LogWarning("Score parent has been destroyed or is missing.");
-                return;
-            }
+                //Delete the previous ones
+                for (int i = 0; i < this.taskHistoriesParent.transform.childCount; i++)
+                {
+                    Destroy(this.taskHistoriesParent.transform.GetChild(i).gameObject);
+                }
+                this.taskHistoriesParent.transform.DetachChildren();
 
-            //Delete the previous ones
-            for (int i = 0; i < this.taskHistoriesParent.transform.childCount; i++)
-            {
-                Destroy(this.taskHistoriesParent.transform.GetChild(i).gameObject);
+                foreach (var item in taskHistories)
+                {
+                    TaskHistoryHandler leaderboardItem = Instantiate(taskHistoryItemPrefab, taskHistoriesParent.transform).GetComponent<TaskHistoryHandler>();
+                    leaderboardItem.Init(item);
+                }
             }
-            this.taskHistoriesParent.transform.DetachChildren();
-
-            foreach (var item in taskHistories)
+            catch (MissingReferenceException)
             {
-                TaskHistoryHandler leaderboardItem = Instantiate(taskHistoryItemPrefab, taskHistoriesParent.transform).GetComponent<TaskHistoryHandler>();
-                leaderboardItem.Init(item);
+                throw;
             }
         }
     }
--- Assets/Scripts/Client/MainPage/WindowHandlers/TaskOpenPanelController.cs
+++ Assets/Scripts/Client/MainPage/WindowHandlers/TaskOpenPanelController.cs
@@ -7,44 +7,84 @@ using UnityEngine.UI;
 
 namespace MainPage
 {
+    /// <summary>
+    /// Controller for displaying, editing and creating tasks
+    /// </summary>
     public class TaskOpenPanelController : ThreadSafeMonoBehaviour
     {
+        /// <summary>
+        /// The current task which is being displayed/edited
+        /// </summary>
         [HideInInspector]
         public PlayerTask CurrentTask { get; private set; } = new PlayerTask() { ID = -1 };
 
+        /// <summary>
+        /// Event for when the window is closed
+        /// </summary>
         [HideInInspector]
         public EventHandler<TaskClosedEventArgs> TaskClosedEventHandler;
 
-        [SerializeField]
-        private GameObject tasksOpenPanel;
-
+        /// <summary>
+        /// Input for the task's name
+        /// </summary>
         [SerializeField]
         private TMP_InputField taskNameInput;
 
+        /// <summary>
+        /// Input dropdown for the task intervals
+        /// </summary>
         [SerializeField]
         private TMP_Dropdown taskIntervals;
 
+        /// <summary>
+        /// Input for the task's description
+        /// </summary>
         [SerializeField]
         private TMP_InputField descriptionInput;
 
+        /// <summary>
+        /// Make the task good
+        /// </summary>
         [SerializeField]
         private Button goodTaskButton;
 
+        /// <summary>
+        /// Make the task bad
+        /// </summary>
         [SerializeField]
         private Button badHabitButton;
 
+        /// <summary>
+        /// Reference to the ui controller
+        /// </summary>
         [SerializeField]
         private MainWindowController uIController;
 
+        /// <summary>
+        /// Delete button for the task
+        /// </summary>
         [SerializeField]
         private Button deleteTaskButton;
 
+        /// <summary>
+        /// Is the task a new one
+        /// </summary>
         private bool isNewTask = false;
 
+        /// <summary>
+        /// Task intervals
+        /// </summary>
         public static readonly int[] TASKINTERVALS = { 60, 120, 240, 1440, 2880, 10080, 20160, 40320 };
 
+        /// <summary>
+        /// What was the task when the task was opened
+        /// so we can revert back on cancel
+        /// </summary>
         private PlayerTask playerTaskOnOpen;
 
+        /// <summary>
+        /// Open up the panel
+        /// </summary>
         public void OpenUp()
         {
             if (this.CurrentTask is not null)
@@ -57,12 +97,19 @@ namespace MainPage
             }
         }
 
+        /// <summary>
+        /// Open's the panel with the task's values added also
+        /// </summary>
+        /// <param name="taskContainer">The task to load</param>
+        /// <param name="type">The type of the task</param>
         public void OpenUp(PlayerTask taskContainer = null, TaskType type = TaskType.GoodTask)
         {
+            //If the task is null create a dummy otherwise load it
             if (taskContainer is not null)
             {
                 this.CurrentTask = taskContainer;
                 this.isNewTask = false;
+                //Create a deep copy
                 this.playerTaskOnOpen = new PlayerTask(taskContainer);
             }
             else
@@ -74,7 +121,6 @@ namespace MainPage
             }
 
             taskNameInput.text = this.CurrentTask.TaskName;
-
             descriptionInput.text = this.CurrentTask.Description;
 
             for (int i = 0; i < TASKINTERVALS.Length; i++)
@@ -85,14 +131,15 @@ namespace MainPage
                     break;
                 }
             }
-
+            //Show/Hide the delete button
             deleteTaskButton.gameObject.SetActive(!isNewTask);
-
-            tasksOpenPanel.SetActive(true);
-
+            this.gameObject.SetActive(true);
             UpdateButtons();
         }
 
+        /// <summary>
+        /// Make this task a bad habit
+        /// </summary>
         public void MakeItBadHabit()
         {
             this.CurrentTask?.ChangeType(TaskType.BadHabit);
@@ -101,6 +148,9 @@ namespace MainPage
             uIController.LoadBadHabits(false);
         }
 
+        /// <summary>
+        /// Make it a good task
+        /// </summary>
         public void MakeItGoodTask()
         {
             this.CurrentTask?.ChangeType(TaskType.GoodTask);
@@ -109,10 +159,14 @@ namespace MainPage
             uIController.LoadGoodTasks(false);
         }
 
+        /// <summary>
+        /// Cancel this edit/task creation
+        /// </summary>
         public void Cancel()
         {
             this.TaskClosedEventHandler?.Invoke(this, new TaskClosedEventArgs(false));
-            tasksOpenPanel.SetActive(false);
+            this.gameObject.SetActive(false);
+            //If it wasn't a new task revert it to the previous state
             if (this.CurrentTask.ID != -1)
             {
                 if (playerTaskOnOpen is not null)
@@ -133,8 +187,12 @@ namespace MainPage
             this.CurrentTask = new PlayerTask() { ID = -1 };
         }
 
+        /// <summary>
+        /// Save the task to the server
+        /// </summary>
         public void Save()
         {
+            //Collect the values from the inputs
             CollectFromFields();
 
             TaskRequest taskRequest = new TaskRequest()
@@ -144,7 +202,7 @@ namespace MainPage
                 PeriodRate = (int)CurrentTask.PeriodRate,
                 TaskType = Convert.ToBoolean(CurrentTask.TaskType)
             };
-
+            //If it is a new one call the create endpoint for the task otherwise the update one
             if (isNewTask)
             {
                 CoroutineRunner.RunCoroutine(Server.SendPostRequest<Thesis_backend.Data_Structures.PlayerTask>(ServerConfig.PATHFORTASKCREATE, taskRequest, SavedTask, onFailedAction: uIController.ShowTaskFail));
@@ -155,14 +213,16 @@ namespace MainPage
             }
         }
 
+        /// <summary>
+        /// When the task was stored on the server show it also visually and close the panel
+        /// </summary>
+        /// <param name="savedTask">The Task which was recieved from the server</param>
         private void SavedTask(Thesis_backend.Data_Structures.PlayerTask savedTask)
         {
             this.TaskClosedEventHandler?.Invoke(this, new TaskClosedEventArgs(true));
-            tasksOpenPanel.SetActive(false);
-            bool isNewTask = CurrentTask.ID == -1;
-
+            this.gameObject.SetActive(false);
             this.CurrentTask = savedTask;
-
+            //If it was a new task create it's prefab otherwise just update it
             if (isNewTask)
             {
                 uIController.CreateTask(this.CurrentTask);
@@ -172,30 +232,41 @@ namespace MainPage
                 uIController.UpdateTaskLabel(CurrentTask.ID);
             }
             uIController.SortingChanged();
-
             this.CurrentTask = new PlayerTask() { ID = -1 };
         }
 
+        /// <summary>
+        /// Send a delete task request to the server
+        /// </summary>
         public void DeleteTask()
         {
             CoroutineRunner.RunCoroutine(Server.SendDeleteRequest<string>(ServerConfig.PATHFORTASKDELETE(CurrentTask.ID), onComplete: DeletedTask, onFailedAction: uIController.ShowTaskFail));
         }
 
+        /// <summary>
+        /// After the task were deleted from the server delete it also from the client
+        /// </summary>
+        /// <param name="result">The result of the deletion</param>
         private void DeletedTask(string result)
         {
             uIController.RemoveTask(CurrentTask.ID);
             Cancel();
         }
 
+        /// <summary>
+        /// Collect the values from the inputs
+        /// </summary>
         private void CollectFromFields()
         {
             string taskName = taskNameInput.text;
             string description = descriptionInput.text;
             TaskIntervals taskInterval = (TaskIntervals)TASKINTERVALS[taskIntervals.value];
-
             CurrentTask.UpdateValues(taskName, CurrentTask.TaskType, taskInterval, description);
         }
 
+        /// <summary>
+        /// Update the buttons with the appropriate color based on the current task
+        /// </summary>
         private void UpdateButtons()
         {
             if (!this.CurrentTask.TaskType)
