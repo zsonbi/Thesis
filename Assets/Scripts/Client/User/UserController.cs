f28eb4c107ee0f62f3f8182defaf6903485ff5be
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-10-06 22:16:55 +0200

    Documented tthe user namespace

--- Assets/Scripts/Client/User/UserController.cs
+++ Assets/Scripts/Client/User/UserController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
@@ -8,175 +7,194 @@ using System.Net.Mail;
 using User;
 using Thesis_backend.Data_Structures;
 
-public class UserController : ThreadSafeMonoBehaviour
+namespace User
 {
     /// <summary>
-    /// The login controls
+    /// Handles the user actions such as registering and login
     /// </summary>
-    [SerializeField]
-    public GameObject LoginPanel;
-
-    /// <summary>
-    /// The register controls
-    /// </summary>
-    [SerializeField]
-    public GameObject RegisterPanel;
-
-    [SerializeField]
-    public ModalWindow ModalWindow;
-
-    // Start is called before the first frame update
-    private void Start()
+    public class UserController : ThreadSafeMonoBehaviour
     {
-        //Cap the fps to 60
-        Application.targetFrameRate = 60;
-        QualitySettings.vSyncCount = 0;
+        /// <summary>
+        /// The login controls
+        /// </summary>
+        [SerializeField]
+        public GameObject LoginPanel;
 
-        //So the user doesn't need to log in if not necessary
-        CheckLoggedIn();
-    }
+        /// <summary>
+        /// The register controls
+        /// </summary>
+        [SerializeField]
+        private GameObject registerPanel;
 
-    /// <summary>
-    /// Send a request to check if the user has logged in already
-    /// </summary>
-    private void CheckLoggedIn()
-    {
-        if (UserData.Instance.LoggedIn)
+        /// <summary>
+        /// Modal window to alert the user
+        /// </summary>
+        [SerializeField]
+        private ModalWindow modalWindow;
+
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        private void Start()
         {
-            MoveToMainScence();
-            return;
+            //Cap the fps to 60
+            Application.targetFrameRate = 60;
+            QualitySettings.vSyncCount = 0;
+
+            //Check if the user is logged in, so the user doesn't need to log in if not necessary
+            CheckLoggedIn();
         }
 
-        WWWForm form = new WWWForm();
-        CoroutineRunner.RunCoroutine(Server.SendGetRequest<Thesis_backend.Data_Structures.User>(ServerConfig.PATHFORCHECKLOGGEDIN, LoggedIn));
-    }
-
-    /// <summary>
-    /// Send a login request with the login panel's fields
-    /// </summary>
-    public void SendLogin()
-    {
-        TMPro.TMP_InputField[] fields = LoginPanel.GetComponentsInChildren<TMPro.TMP_InputField>();
-
-        UserLoginRequest userLoginRequest = new UserLoginRequest()
+        /// <summary>
+        /// Send a request to check if the user has logged in already
+        /// </summary>
+        private void CheckLoggedIn()
         {
-            UserIdentification = fields[0].text,
-            Password = fields[1].text,
-        };
+            //If the user is still cached in the RAM move him to the main page
+            if (UserData.Instance.LoggedIn)
+            {
+                MoveToMainScence();
+                return;
+            }
 
-        CoroutineRunner.RunCoroutine(Server.SendPostRequest<Thesis_backend.Data_Structures.User>(ServerConfig.PATHFORLOGIN, userLoginRequest, LoggedIn, onFailedAction: ShowLoginError));
-    }
-
-    /// <summary>
-    /// When the login request's was responded log the user in
-    /// </summary>
-    /// <param name="result">The server's response</param>
-    private void LoggedIn(Thesis_backend.Data_Structures.User result)
-    {
-        UserData.Instance.Init(result);
-        CoroutineRunner.RunCoroutine(MoveToMainScence());
-    }
-
-    /// <summary>
-    /// Send a register request with the register panel's fields
-    /// </summary>
-    public void SendRegister()
-    {
-        TMPro.TMP_InputField[] fields = RegisterPanel.GetComponentsInChildren<TMPro.TMP_InputField>();
-
-        if (!IsValidEmail(fields[0].text))
-        {
-            ModalWindow.Show("Register error", "This is not a valid email!");
-
-            return;
+            CoroutineRunner.RunCoroutine(Server.SendGetRequest<Thesis_backend.Data_Structures.User>(ServerConfig.PATHFORCHECKLOGGEDIN, LoggedIn));
         }
-        if (fields[2].text != fields[3].text)
+
+        /// <summary>
+        /// Send a login request with the login panel's fields
+        /// </summary>
+        public void SendLogin()
         {
-            ModalWindow.Show("Register error", "Passwords does not match!");
-            return;
+            TMPro.TMP_InputField[] fields = LoginPanel.GetComponentsInChildren<TMPro.TMP_InputField>();
+
+            UserLoginRequest userLoginRequest = new UserLoginRequest()
+            {
+                UserIdentification = fields[0].text,
+                Password = fields[1].text,
+            };
+
+            CoroutineRunner.RunCoroutine(Server.SendPostRequest<Thesis_backend.Data_Structures.User>(ServerConfig.PATHFORLOGIN, userLoginRequest, LoggedIn, onFailedAction: ShowLoginError));
         }
-        UserRequest userRequest = new UserRequest()
+
+        /// <summary>
+        /// When the login request's was responded log the user in
+        /// </summary>
+        /// <param name="result">The server's response</param>
+        private void LoggedIn(Thesis_backend.Data_Structures.User result)
         {
-            UserName = fields[1].text,
-            Email = fields[0].text,
-            Password = fields[2].text,
-        };
-
-        CoroutineRunner.RunCoroutine(Server.SendPostRequest<Thesis_backend.Data_Structures.User>(ServerConfig.PATHFORREGISTER, userRequest, Registered, onFailedAction: ShowRegisterError));
-    }
-
-    private void ShowRegisterError(string content)
-    {
-        ModalWindow.Show("Register error", content);
-    }
-
-    private void ShowLoginError(string content)
-    {
-        ModalWindow.Show("Login error", content);
-    }
-
-    /// <summary>
-    /// When the login request's was responded register the user and log him in
-    /// </summary>
-    /// <param name="result">The server's response</param>
-    private void Registered(Thesis_backend.Data_Structures.User loggedInUser)
-    {
-        UserData.Instance.Init(loggedInUser);
-        CoroutineRunner.RunCoroutine(MoveToMainScence());
-    }
-
-    /// <summary>
-    /// Move from the user creation and login scene
-    /// </summary>
-    private IEnumerator MoveToMainScence()
-    {
-        AsyncOperation loading = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
-
-        while (!loading.isDone)
-        {
-            yield return null;
+            UserData.Instance.Init(result);
+            CoroutineRunner.RunCoroutine(MoveToMainScence());
         }
-    }
 
-    /// <summary>
-    /// Check if the email is valid
-    /// </summary>
-    /// <param name="emailaddress">The email to validate</param>
-    /// <returns>true-correct false-sus</returns>
-    public bool IsValidEmail(string emailaddress)
-    {
-        try
+        /// <summary>
+        /// Send a register request with the register panel's fields
+        /// </summary>
+        public void SendRegister()
         {
-            if (emailaddress == "")
+            TMPro.TMP_InputField[] fields = registerPanel.GetComponentsInChildren<TMPro.TMP_InputField>();
+            //Validate the register inputs locally
+            if (!IsValidEmail(fields[0].text))
+            {
+                modalWindow.Show("Register error", "This is not a valid email!");
+
+                return;
+            }
+            if (fields[2].text != fields[3].text)
+            {
+                modalWindow.Show("Register error", "Passwords does not match!");
+                return;
+            }
+            UserRequest userRequest = new UserRequest()
+            {
+                UserName = fields[1].text,
+                Email = fields[0].text,
+                Password = fields[2].text,
+            };
+            //Send the request
+            CoroutineRunner.RunCoroutine(Server.SendPostRequest<Thesis_backend.Data_Structures.User>(ServerConfig.PATHFORREGISTER, userRequest, Registered, onFailedAction: ShowRegisterError));
+        }
+
+        /// <summary>
+        /// Show the register error on the modal window
+        /// </summary>
+        /// <param name="content">What to show</param>
+        private void ShowRegisterError(string content)
+        {
+            modalWindow.Show("Register error", content);
+        }
+
+        /// <summary>
+        /// Show the login error on the modal window
+        /// </summary>
+        /// <param name="content">What to show</param>
+        private void ShowLoginError(string content)
+        {
+            modalWindow.Show("Login error", content);
+        }
+
+        /// <summary>
+        /// When the register request was responded successfully register the user and log him in
+        /// </summary>
+        /// <param name="loggedInUser">The registered user's data</param>
+        private void Registered(Thesis_backend.Data_Structures.User loggedInUser)
+        {
+            UserData.Instance.Init(loggedInUser);
+            CoroutineRunner.RunCoroutine(MoveToMainScence());
+        }
+
+        /// <summary>
+        /// Move from the user creation and login scene
+        /// </summary>
+        private IEnumerator MoveToMainScence()
+        {
+            AsyncOperation loading = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+
+            while (!loading.isDone)
+            {
+                yield return null;
+            }
+        }
+
+        /// <summary>
+        /// Check if the email is valid
+        /// </summary>
+        /// <param name="emailaddress">The email to validate</param>
+        /// <returns>true-correct false-sus</returns>
+        public bool IsValidEmail(string emailaddress)
+        {
+            try
+            {
+                if (emailaddress == "")
+                {
+                    return false;
+                }
+
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
             {
                 return false;
             }
-
-            MailAddress m = new MailAddress(emailaddress);
-
-            return true;
         }
-        catch (FormatException)
+
+        /// <summary>
+        /// Change panels
+        /// </summary>
+        public void MoveToRegisterWindow()
         {
-            return false;
+            registerPanel.SetActive(true);
+            LoginPanel.SetActive(false);
         }
-    }
 
-    /// <summary>
-    /// Change panels
-    /// </summary>
-    public void MoveToRegisterWindow()
-    {
-        RegisterPanel.SetActive(true);
-        LoginPanel.SetActive(false);
-    }
-
-    /// <summary>
-    /// Change panels
-    /// </summary>
-    public void MoveToLoginWindow()
-    {
-        RegisterPanel.SetActive(false);
-        LoginPanel.SetActive(true);
+        /// <summary>
+        /// Change panels
+        /// </summary>
+        public void MoveToLoginWindow()
+        {
+            registerPanel.SetActive(false);
+            LoginPanel.SetActive(true);
+        }
     }
 }
--- Assets/Scripts/Client/User/UserData.cs
+++ Assets/Scripts/Client/User/UserData.cs
@@ -1,35 +1,93 @@
-using Config;
 using System;
-using System.Collections.Generic;
 using Thesis_backend.Data_Structures;
-using UnityEngine;
 
 namespace User
 {
+    /// <summary>
+    /// Singleton class for storing the user's data
+    /// </summary>
     public class UserData
     {
+        /// <summary>
+        /// Singleton private instance
+        /// </summary>
         private static UserData instance = null;
+
+        /// <summary>
+        /// Instance getter
+        /// </summary>
         public static UserData Instance { get => GetOrCreate(); }
 
+        /// <summary>
+        /// The user's username
+        /// </summary>
         public string Username { get; private set; }
+
+        /// <summary>
+        /// The user's email
+        /// </summary>
         public string Email { get; private set; }
+
+        /// <summary>
+        /// The user's id
+        /// </summary>
         public long Id { get; private set; }
-        public UserSettings SettingsId { get; private set; }
+
+        /// <summary>
+        /// The user's settings
+        /// </summary>
+        public UserSettings UserSettings { get; private set; }
+
+        /// <summary>
+        /// The user's game data
+        /// </summary>
         public Thesis_backend.Data_Structures.Game Game { get; private set; }
+
+        /// <summary>
+        /// When did the user last logged in
+        /// </summary>
         public DateTime LastLoggedIn { get; private set; }
+
+        /// <summary>
+        /// When did the user registered
+        /// </summary>
         public DateTime Registered { get; private set; }
+
+        /// <summary>
+        /// Is the user currently logged in
+        /// </summary>
         public bool LoggedIn { get; private set; } = false;
+
+        /// <summary>
+        /// Total accumulation of task score
+        /// </summary>
         public long TotalScore { get; private set; } = 0;
+
+        /// <summary>
+        /// Current spendable task score amount
+        /// </summary>
         public long CurrentTaskScore { get; private set; } = 0;
+
+        /// <summary>
+        /// How many good tasks did the user completed since creation
+        /// </summary>
         public int CompletedGoodTasks { get; set; } = 0;
+
+        /// <summary>
+        /// How many bad habits the user completed since creation
+        /// </summary>
         public int CompletedBadTasks { get; set; } = 0;
 
+        /// <summary>
+        /// Initialises the user instance with proper data
+        /// </summary>
+        /// <param name="loggedInUser">The logged in user's data from the server</param>
         public void Init(Thesis_backend.Data_Structures.User loggedInUser)
         {
             Username = loggedInUser.Username;
             Email = loggedInUser.Email;
             Id = loggedInUser.ID;
-            SettingsId = loggedInUser.UserSettings;
+            UserSettings = loggedInUser.UserSettings;
             Game = loggedInUser.Game;
             LastLoggedIn = loggedInUser.LastLoggedIn;
             Registered = loggedInUser.Registered;
@@ -40,6 +98,10 @@ namespace User
             this.CompletedBadTasks = loggedInUser.CompletedBadTasks;
         }
 
+        /// <summary>
+        /// Get's the instance or create it if it doesn't exist yet
+        /// </summary>
+        /// <returns>The instance</returns>
         private static UserData GetOrCreate()
         {
             if (instance is null)
@@ -50,19 +112,28 @@ namespace User
             return instance;
         }
 
+        /// <summary>
+        /// Update the user's task score
+        /// </summary>
+        /// <param name="newAmount">The new amount to update to</param>
         public void UpdateTaskScore(long newAmount)
         {
             this.CurrentTaskScore = newAmount;
         }
 
+        /// <summary>
+        /// Creates the user data
+        /// </summary>
         private UserData()
         {
         }
 
+        /// <summary>
+        /// Signal's that the user is logged out
+        /// </summary>
         public void Logout()
         {
             LoggedIn = false;
-            //StartCoroutine(Server.SendDeleteRequest<string>(ServerConfig.PATHFORLOGOUT));
         }
     }
 }
