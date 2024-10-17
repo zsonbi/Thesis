3375bc6856bba0e06573842c1cda493c66b3bcae
Dér Zsombor Zsolt <zsomborder@gmail.com>
2024-10-17 14:30:32 +0200

    Documented the game's ui

--- Assets/Scripts/Client/Game/UI/LeaderboardItem.cs
+++ Assets/Scripts/Client/Game/UI/LeaderboardItem.cs
@@ -6,14 +6,28 @@ namespace Game
 {
     namespace UI
     {
+        /// <summary>
+        /// A single leaderboard item
+        /// </summary>
         public class LeaderboardItem : ThreadSafeMonoBehaviour
         {
+            /// <summary>
+            /// Reference to the player name's text
+            /// </summary>
             [SerializeField]
             private TMP_Text playerNameText;
 
+            /// <summary>
+            /// Reference to the score text
+            /// </summary>
             [SerializeField]
             private TMP_Text scoreText;
 
+            /// <summary>
+            /// Initializes the item
+            /// </summary>
+            /// <param name="playerName">The achieved player's name</param>
+            /// <param name="score">The achieved score</param>
             public void Init(string playerName, int score)
             {
                 this.playerNameText.text = playerName;
--- Assets/Scripts/Client/Game/World/Chunk.cs
+++ Assets/Scripts/Client/Game/World/Chunk.cs
@@ -17,7 +17,7 @@ namespace Game
     namespace World
     {
         /// <summary>
-        /// Used for generating a new world
+        /// Stores a single chunk of the world
         /// </summary>
         public class Chunk : ThreadSafeMonoBehaviour
         {
@@ -27,29 +27,56 @@ namespace Game
             [Header("Offset of perlin noise on Z axis")]
             public float ZOffset = 0f;
 
+            /// <summary>
+            /// Should it randomize the perlin noise offset
+            /// </summary>
             [Header("Randomize Offset")]
             public bool RandomizeOffset = false;
 
+            /// <summary>
+            /// Reference to the straight road prefab
+            /// </summary>
             [SerializeField]
             public GameObject straightRoadPrefab;
 
+            /// <summary>
+            /// Reference to the curved road prefab
+            /// </summary>
             [SerializeField]
             private GameObject curvedRoadPrefab;
 
+            /// <summary>
+            /// Reference to the three way road prefab
+            /// </summary>
             [SerializeField]
             private GameObject threeWayRoadPrefab;
 
+            /// <summary>
+            /// Reference to the cross roads prefab (4 way)
+            /// </summary>
             [SerializeField]
             private GameObject crossRoadsPrefab;
 
+            /// <summary>
+            /// Reference to the grass prefab
+            /// </summary>
             [SerializeField]
             public GameObject grassPrefab;
 
+            /// <summary>
+            /// Reference to the coin prefab
+            /// </summary>
             [SerializeField]
             public GameObject coinPrefab;
 
+            /// <summary>
+            /// The buildings which can be spawn
+            /// </summary>
             private List<Building> buildings;
 
+            /// <summary>
+            /// The probs which can be spawn (objects which are away from the road)
+            /// </summary>
             private List<Prop> props;
 
             /// <summary>
@@ -62,22 +89,59 @@ namespace Game
             /// </summary>
             public int? xSize { get; private set; }
 
+            /// <summary>
+            /// Row index of the chunk in the chunk matrix
+            /// </summary>
             public int Row { get; private set; }
+
+            /// <summary>
+            /// Col index of the chunk in the chunk matrix
+            /// </summary>
             public int Col { get; private set; }
 
-            public bool? Loaded { get => this.gameObject.activeSelf; }
-
+            /// <summary>
+            /// Matrix which stores the roads
+            /// </summary>
             public bool[,] Roads => roadGenerator.RoadMatrix;
 
+            /// <summary>
+            /// Reference to the road generator's edge roads
+            /// </summary>
             public List<EdgeRoadContainer> EdgeRoads { get => roadGenerator?.EdgeRoads; }
 
+            /// <summary>
+            /// The cells of tthe chunk categorized
+            /// </summary>
             private Dictionary<ChunkCellType, List<Vector3>> chunkCells = new Dictionary<ChunkCellType, List<Vector3>>();
+
+            /// <summary>
+            /// Class which generates the road matrix
+            /// </summary>
             private RoadGenerator roadGenerator;
+
+            /// <summary>
+            /// Reference to the parent world
+            /// </summary>
             private GameWorld world;
+
+            /// <summary>
+            /// The road positions
+            /// </summary>
             private List<Vector3Int> roads = new List<Vector3Int>();
+
+            /// <summary>
+            /// Mostly deprecated but can be used to combine the cell's meshes into a one big mesh
+            /// </summary>
             private Dictionary<ChunkCellType, List<GameObject>> objectsToCombine;
+
+            /// <summary>
+            /// Matrix to help the building placements stores road direction and if it is buildable
+            /// </summary>
             private BuildingCell[,] buildingCells;
 
+            /// <summary>
+            /// For single threaded tasks
+            /// </summary>
             [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
             public static void InitUniTaskLoop()
             {
@@ -85,17 +149,18 @@ namespace Game
                 Cysharp.Threading.Tasks.PlayerLoopHelper.Initialize(ref loop);
             }
 
-            // Start is called before the first frame update
+            /// <summary>
+            /// Start when the script is loaded
+            /// </summary>
             private async void Awake()
             {
+                //Load the addressables
                 buildings = new List<Building>();
-                //PlayerLoopHelper.Initialize();
                 var buildingsLoadHandle = Addressables.LoadAssetsAsync<GameObject>("Buildings", BuildingLoaded);
-
                 await buildingsLoadHandle.Task;
+
                 props = new List<Prop>();
                 var propsLoadHandle = Addressables.LoadAssetsAsync<GameObject>("Props", PropLoaded);
-
                 await propsLoadHandle.Task;
 
                 //Load the values from the settings
@@ -108,35 +173,56 @@ namespace Game
                 }
 
                 this.objectsToCombine = new Dictionary<ChunkCellType, List<GameObject>>();
-                Debug.Log("World buildings");
             }
 
-            // Callback for when each asset is loaded
+            /// <summary>
+            /// Callback for when each building asset is loaded
+            /// </summary>
+            /// <param name="building">Loaded building</param>
             private void BuildingLoaded(GameObject building)
             {
                 buildings.Add(building.GetComponent<Building>());
                 buildings.Last().SetAddressableKey($"Buildings/{building.name}.prefab");
             }
 
-            // Callback for when each asset is loaded
+            /// <summary>
+            /// Callback for when each prop asset is loaded
+            /// </summary>
+            /// <param name="prop">Loaded prop</param>
             private void PropLoaded(GameObject prop)
             {
                 props.Add(prop.GetComponent<Prop>());
                 props.Last().SetAddressableKey($"Props/{prop.name}.prefab");
             }
 
+            /// <summary>
+            /// Get the chunk's absolute position in the world
+            /// </summary>
+            /// <returns>The chunk's absolute position in a tuple</returns>
             private (float, float) GetAbsolutePosition()
             {
                 return (Col * GameConfig.CHUNK_SCALE * GameConfig.CHUNK_CELL * GameConfig.CHUNK_SIZE, Row * GameConfig.CHUNK_SCALE * GameConfig.CHUNK_CELL * GameConfig.CHUNK_SIZE);
             }
 
+            /// <summary>
+            /// Show the chunk
+            /// </summary>
             public void Display()
             {
                 this.gameObject.SetActive(true);
             }
 
-            public async Task InitChunk(int xOffset, int zOffset, List<EdgeRoadContainer> edgeRoads, GameWorld world)
+            /// <summary>
+            /// Initialize the chunk
+            /// </summary>
+            /// <param name="col">The column offset</param>
+            /// <param name="row">The row offset</param>
+            /// <param name="edgeRoads"></param>
+            /// <param name="world"></param>
+            /// <returns></returns>
+            public async Task InitChunk(int col, int row, List<EdgeRoadContainer> edgeRoads, GameWorld world)
             {
+                //Wait for the addressables to be loaded
                 while (objectsToCombine is null)
                 {
 #if UNITY_WEBGL
@@ -146,25 +232,29 @@ namespace Game
 #endif
                 }
                 this.world = world;
-                this.Row = zOffset;
-                this.Col = xOffset;
+                this.Row = row;
+                this.Col = col;
+                //Add every type of chunk cell to the dictionary
                 for (ChunkCellType i = 0; i <= ChunkCellType.StraightRoad; i++)
                 {
                     chunkCells.Add(i, new List<Vector3>());
                 }
 
+                //Detroyed check
                 if (Destroyed)
                 {
                     return;
                 }
 
+                //Get the nearby chunks and generate the road matrix
                 Chunk[,] nearbyChunks = await GetNearbyChunks(Row, Col);
                 roadGenerator = new RoadGenerator(GameConfig.CHUNK_SIZE, edgeRoads, nearbyChunks);
 
                 //Create the tiles
                 CreateTiles();
 
-                ////Create the props
+                //Deprecated
+                //Create the props
                 //AddEnviromentObjects();
                 //Combine the objects
                 foreach (var item in objectsToCombine)
@@ -176,21 +266,29 @@ namespace Game
                 }
 
                 //CreateMeshes();
+                //Destroyed check
                 if (Destroyed)
                 {
                     return;
                 }
-                this.gameObject.transform.localPosition = new Vector3(xOffset * GameConfig.CHUNK_SIZE * GameConfig.CHUNK_SCALE * GameConfig.CHUNK_CELL, 0, zOffset * GameConfig.CHUNK_SIZE * GameConfig.CHUNK_SCALE * GameConfig.CHUNK_CELL);
-
+                this.gameObject.transform.localPosition = new Vector3(col * GameConfig.CHUNK_SIZE * GameConfig.CHUNK_SCALE * GameConfig.CHUNK_CELL, 0, row * GameConfig.CHUNK_SIZE * GameConfig.CHUNK_SCALE * GameConfig.CHUNK_CELL);
                 this.gameObject.transform.localScale = new Vector3(GameConfig.CHUNK_SCALE, 1, GameConfig.CHUNK_SCALE);
-                await GenerateBuildings();
+                //Generate the obstacles
+                await SpawnWorldObjects();
             }
 
+            /// <summary>
+            /// Hide the chunk
+            /// </summary>
             public void HideChunk()
             {
                 this.gameObject.SetActive(false);
             }
 
+            /// <summary>
+            /// Gets a random road's position
+            /// </summary>
+            /// <returns></returns>
             public Vector3 GetARandomRoad()
             {
                 if (roads.Count == 0)
@@ -200,17 +298,18 @@ namespace Game
                 return roads[Random.Range(0, roads.Count)];
             }
 
-            //------------------------------------------------------------------------
-            //Load the sizes from the settings
+            /// <summary>
+            /// Load the chunk's sizes
+            /// </summary>
             private void LoadFromSettings()
             {
                 this.xSize = GameConfig.CHUNK_SIZE;
                 this.zSize = GameConfig.CHUNK_SIZE;
             }
 
-            //---------------------------------------------------------------------------
-            // <summary>
-            // Creates the tiles for the world
+            /// <summary>
+            /// Creates the tiles for the chunk
+            /// </summary>
             private void CreateTiles()
             {
                 for (int x = 0; x < xSize; x++)
@@ -224,6 +323,7 @@ namespace Game
 
                         ChunkCellContainer tileType = DetermineTileType(x, z);
                         GameObject created = null;
+                        //Load tthe correct prefab
                         switch (tileType.Type)
                         {
                             case ChunkCellType.StraightRoad:
@@ -255,6 +355,7 @@ namespace Game
                             default:
                                 break;
                         }
+                        //Move it to the proper position and store it
                         if (created != null)
                         {
                             created.transform.localScale = created.transform.localScale * 1.01f;
@@ -279,7 +380,13 @@ namespace Game
                 }
             }
 
-            private ChunkCellContainer DetermineRoadType(int x, int z)
+            /// <summary>
+            /// Determine what type of road it is
+            /// </summary>
+            /// <param name="col">Column index</param>
+            /// <param name="row">Row index</param>
+            /// <returns>The chunk cell container with the correct road type</returns>
+            private ChunkCellContainer DetermineRoadType(int col, int row)
             {
                 int roadCount = 0;
 
@@ -287,14 +394,14 @@ namespace Game
                 int lastIndex = -1;
                 int secondIndex = -1;
 
-                if (x - 1 >= 0 && roadGenerator.RoadMatrix[z, x - 1])
+                if (col - 1 >= 0 && roadGenerator.RoadMatrix[row, col - 1])
                 {
                     roadCount++;
                     firstIndex = 0;
                     lastIndex = 0;
                 }
 
-                if (z + 1 < GameConfig.CHUNK_SIZE && roadGenerator.RoadMatrix[z + 1, x])
+                if (row + 1 < GameConfig.CHUNK_SIZE && roadGenerator.RoadMatrix[row + 1, col])
                 {
                     roadCount++;
 
@@ -309,7 +416,7 @@ namespace Game
                     lastIndex = 1;
                 }
 
-                if (x + 1 < GameConfig.CHUNK_SIZE && roadGenerator.RoadMatrix[z, x + 1])
+                if (col + 1 < GameConfig.CHUNK_SIZE && roadGenerator.RoadMatrix[row, col + 1])
                 {
                     roadCount++;
 
@@ -324,7 +431,7 @@ namespace Game
                     lastIndex = 2;
                 }
 
-                if (z - 1 >= 0 && roadGenerator.RoadMatrix[z - 1, x])
+                if (row - 1 >= 0 && roadGenerator.RoadMatrix[row - 1, col])
                 {
                     roadCount++;
 
@@ -345,11 +452,6 @@ namespace Game
                 }
                 else if (roadCount == 3)
                 {
-                    //if (lastIndex == 3)
-                    //{
-                    //    return new ChunkCellContainer(ChunkCellType.ThreeWay, new Vector3(0, (firstIndex+1) * 90, 0));
-                    //}
-
                     if (firstIndex == 0 && lastIndex == 3)
                     {
                         if (secondIndex == 1)
@@ -401,6 +503,7 @@ namespace Game
                 {
                     return new ChunkCellContainer(ChunkCellType.Grass, Vector3.zero);
                 }
+                //If later we want other types of biomes
                 else if (noise > 0.05f)
                 {
                     return new ChunkCellContainer(ChunkCellType.Sand, Vector3.zero);
@@ -411,7 +514,10 @@ namespace Game
                 }
             }
 
-            public async Task GenerateBuildings()
+            /// <summary>
+            /// Spawns the buildings and the props on a seperate thread
+            /// </summary>
+            public async Task SpawnWorldObjects()
             {
                 buildingCells = new BuildingCell[GameConfig.CHUNK_SIZE, GameConfig.CHUNK_SIZE];
 #if UNITY_WEBGL
@@ -423,6 +529,9 @@ namespace Game
                 CoroutineRunner.RunCoroutine(AddEnvironmentProps());
             }
 
+            /// <summary>
+            /// Generate the building cells matrix so we know which way the roads are
+            /// </summary>
             private void GenerateCellMatrix()
             {
                 foreach (var road in roads)
@@ -495,6 +604,9 @@ namespace Game
                 }
             }
 
+            /// <summary>
+            /// Add the buildings which are next to the roads
+            /// </summary>
             private IEnumerator AddRoadBuildings()
             {
                 for (int i = 1; i < GameConfig.CHUNK_SIZE - 1; i++)
@@ -552,6 +664,12 @@ namespace Game
                 yield return null;
             }
 
+            /// <summary>
+            /// Lock the cell and try to place it
+            /// </summary>
+            /// <param name="index">The building index</param>
+            /// <param name="row">The row position</param>
+            /// <param name="col">The column position</param>
             private void LockAndTryToPlaceBuilding(int index, int row, int col)
             {
                 if (buildingCells[row, col].GotRoadNext && buildingCells[row, col].Buildable)
@@ -616,6 +734,12 @@ namespace Game
                 }
             }
 
+            /// <summary>
+            /// Gets tthe nearby chunks
+            /// </summary>
+            /// <param name="row">The row index of the center chunk</param>
+            /// <param name="col">The col index of the center chunk</param>
+            /// <returns>3*3 matrix of chunks</returns>
             private async Task<Chunk[,]> GetNearbyChunks(int row, int col)
             {
                 Chunk[,] nearbyChunks = new Chunk[3, 3];
